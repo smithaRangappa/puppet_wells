a33225788485dbd77a147723ac964cb1651119b9
smithaRangappa <41051229+smithaRangappa@users.noreply.github.com>
2018-07-11 04:44:25 +0000

    Update params.pp

--- params.pp
+++ params.pp
@@ -23,3 +23,12 @@ class shh(
   class { 'ssh::install':: } ->
   class { 'ssh::service':: }
 }
+=====
+class ssh::install(
+  String $package_name = $::ssh::package_name,
+  ) {
+  package { 'ssh-package':
+  ensure => present,
+  name => $package_name,
+  }
+}
